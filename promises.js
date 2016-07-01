1f149ffd6d8721cbd9e474a2d2c59930b854a63b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-01 09:28:10 +0200

    Use Bluebird Promises library to retrieve Github avatar

--- promises.js
+++ promises.js
@@ -6,6 +6,7 @@ var github = new GitHubApi({
 	version: '3.0.0'
 });
 
+//Using callbacks
 var getUserAvatarWithCallback = function(user, callback) {
 	github.search.users({q: user}, function(err, res) {
 		if (err) { callback(err, null); }
@@ -18,4 +19,27 @@ var getUserAvatarWithCallback = function(user, callback) {
 
 getUserAvatarWithCallback('sanlouise', function(err, avatar) {
 	console.log('Got url with callbacks!', avatar)
+});
+
+
+//Using Promises, now with Bluebird
+var getUserAvatarWithBlueBird = function(user) {
+	return new Promise(function(resolve, reject) {
+		github.search.users({q :user }, function(err, res) {
+			if (err) { reject(err); }
+			else {
+
+				var avatarUrl = res.items[0].avatar_url;
+				resolve(avatarUrl);
+			}
+		});
+	});
+};
+
+getUserAvatarWithBlueBird('sanlouise')
+.then(function(avatarUrl) {
+	console.log('Got url with Bluebird', avatarUrl);
 })
+.catch(function(error) {
+	console.log('Error with Bluebird', error);
+});
